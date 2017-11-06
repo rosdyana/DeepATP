d742df0d556a7540cd464024b4dd2f406b4cb329
rosdyana <rosdyana.kusuma@gmail.com>
2017-11-06 10:35:01 +0800

    *fixed path

--- resources/views/submission.blade.php
+++ resources/views/submission.blade.php
@@ -13,7 +13,7 @@
 @section('content')
 <div class="wrapper">
   <div class="page-header page-header-small" style="max-height: 80px;">
-    <div class="page-header-image" data-parallax="true" style="background-image: url('../assets/img/bg.png');">
+    <div class="page-header-image" data-parallax="true" style="background-image: url('assets/img/bg.png');">
     </div>
   </div>
   <div class="section section-about-us">
