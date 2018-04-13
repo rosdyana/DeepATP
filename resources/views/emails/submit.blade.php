f0f9dc82d12c92dc9e1e8215d7fec879de4950ff
rosdyana <rosdyana.kusuma@gmail.com>
2018-04-13 12:07:09 +0800

    change submit task msg

--- resources/views/emails/submit.blade.php
+++ resources/views/emails/submit.blade.php
@@ -1,6 +1,5 @@
-@component('mail::message') # Dear Mr/Ms {{ $name }}, Thank you for using our website, your ticket number is #{{ $id }} Thank
-you for using our web-service, the results will be informed to you by email when it's done. I hope you will send more protein
-samples so that we can enrich our database and improve the accuracy of the system. @component('mail::button', ['url' => url('task/'.
-$id) ]) Your detailed task @endcomponent Thanks,
+@component('mail::message') # Dear Mr/Ms {{ $name }}, Thank you for using our website, your ticket number is #{{ $id }}.
+I hope you will send more protein samples so that we can enrich our database and improve the accuracy of the system. @component('mail::button',
+['url' => url('task/'. $id) ]) Your detailed task @endcomponent Thanks,
 
 <br> {{ config('app.name') }} @endcomponent
